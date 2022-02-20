c6d3006c520cc2fee52b60f67fd7459f1fc36d44
eduuh <edwinkamaumuraya0@outlook.com>
2022-02-20 22:05:58 +0300

    update the keyboard layout..
    removing the central page-up and page-down and replacing it with braces
    
    Update the script to account for presence of files

--- keyboard.sh
+++ keyboard.sh
@@ -3,5 +3,21 @@
 # This script will replace the default colemak layout
 # Using a hard link to link to the right config file
 
-sudo mv  /usr/share/X11/xkb/symbols/us /usr/share/X11/xkb/symbols/us.bak
-sudo ln  ./Arch/us /usr/share/X11/xkb/symbols/us
+KEYBOARD=/usr/share/X11/xkb/symbols/Keyboard.bak
+
+if [ -f "$KEYBOARD" ]; then
+   echo "removed old backup keyboard $KEYBOARD"
+   sudo rm -rf $KEYBOARD
+ fi
+
+echo "Creating a new backup file"
+sudo mv  /usr/share/X11/xkb/symbols/us $KEYBOARD
+
+ARCH=/usr/share/X11/xkb/symbols/us
+
+if [ -f "$KEYBOARD" ]; then
+   echo "Removing existing hard link"
+fi
+
+echo "Creating a new hard Link"
+sudo ln ./Arch/us $ARCH
